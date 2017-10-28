8fe664ae71d4b176225cc4eb5211b0eb7c616059
ubashadowzero <32531713+ubashadowzero@users.noreply.github.com>
2017-10-27 19:54:46 -0500

    Actualización de archivos

--- js/app.js
+++ js/app.js
@@ -1,26 +1,6 @@
 function init(){
 
-	var open_menu = document.querySelector("#openMenu");
-	var close_menu = document.querySelector("#closeMenu");
-	var menu = document.querySelector('#menu');
 
-	var openMenu = function (event){
-		open_menu.style.display = 'none';
-		menu.style.display = 'block';
-		menu.style.position = 'absolute';
-		close_menu.style.display = 'block';
-		event.stopPropagation();
-	}
-
-	var closeMenu = function (event){
-		open_menu.style.display = 'block';
-		menu.style.display = 'none';
-		close_menu.style.display = 'none';
-		event.stopPropagation();
-	}
-
-	open_menu.addEventListener('click', openMenu);
-	close_menu.addEventListener('click', closeMenu);
 
 }
 
