c433cd02ffe83536626422f1a2bd8ef45ee41659
Uba0303 <ubaldo.diaz@hallmg.com>
2017-10-30 11:03:37 -0600

    Acciones del menu con mobile

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 function init(){
 
+	var openMenu = document.querySelector("#openMenu");
+	var closeMenu = document.querySelector("#closeMenu");
+	var navMenu = document.querySelector(".header__show-nav");
 
+
+	var actionOpenMenu = function actionOpenMenu(){
+		navMenu.style.visibility = "visible";
+		openMenu.style.visibility = 'hidden';
+		closeMenu.style.visibility = 'visible';
+	}
+
+
+	var actionCloseMenu = function actionCloseMenu(){
+		navMenu.style.visibility = 'hidden';
+		openMenu.style.visibility = 'visible';
+		closeMenu.style.visibility = 'hidden';
+	}
+
+	openMenu.addEventListener( 'click', actionOpenMenu );
+	closeMenu.addEventListener( 'click', actionCloseMenu);
 
 }
 
